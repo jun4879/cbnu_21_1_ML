7d84aa742e04d84ef91f307d9feae6a301935bfe
KIM JUN TAE <57665924+jun4879@users.noreply.github.com>
2021-05-27 15:02:49 +0900

    단어 사전 만들기 작업

---
+++ Term_Project_Chatbot_Model/test/load_dictionary_test.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('../utils')
+
+import pickle
+from preprocess import Preprocess
+
+f = open('../train_tools/dict/chatbot_dict.bin', 'rb')
+word_index = pickle.load(f)
+f.close()
+
+sentence = "오늘 오후 5시 30분에 닭고기를 먹고 싶어 ㅎㅎㅎ"
+
+# 전처리 객체 생성
+p = Preprocess(userdic='../utils/user_dic.tsv')
+
+# 형태소 분석기 실행
+pos = p.pos(sentence)
+
+# 품사 태그와 같이 키워드 출력
+keywords = p.get_keywords(pos, without_tag=True)
+for word in keywords:
+    try:
+        print(word, word_index[word])
+    except KeyError:
+        # 해당 단어가 사전에 없는 경우 OOV 처리
+        print(word, word_index['OOV'])
