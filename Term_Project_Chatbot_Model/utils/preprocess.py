cd789d176222ce044d331d4f2ecccc03da4209b5
KIM JUN TAE <57665924+jun4879@users.noreply.github.com>
2021-05-27 15:10:15 +0900

    Update preprocess.py

--- Term_Project_Chatbot_Model/utils/preprocess.py
+++ Term_Project_Chatbot_Model/utils/preprocess.py
@@ -1,7 +1,9 @@
+import pickle
+
 from konlpy.tag import Komoran
 
 class Preprocess:
-    def __init__(self, userdic=None):
+    def __init__(self, word2index_dic='', userdic=None):
         # 형태소 분석기 komoran 초기화
         self.komoran = Komoran(userdic=userdic) # userdic 인자에 사용자 정의 사전 파일 경로 입력하여 추가 가능
         # 제외할 품사 (참조 : https://komorandocs.readthedocs.io/ko/latest/firststep/postypes.html)
@@ -15,6 +17,13 @@ class Preprocess:
             # 접미사 : 명사파생접미사, 동사파생접미사, 형용사파생접미사
             'XSN', 'XSV', 'XSA'
         ]
+        # 단어 인덱스 사전 불러오기
+        if (word2index_dic != ''):
+            f = open(word2index_dic, 'rb')
+            self.word_index = pickle.load(f)
+            f.close()
+        else :
+            self.word_index = None
 
     # 형태소 분석기 POS Tagger
     def pos(self, sentence):
@@ -28,3 +37,16 @@ class Preprocess:
             if f(p[1]) is False:
                 word_list.append(p if without_tag is False else p[0])
         return word_list
+
+    # 키워드를 단어 인덱스 시퀀스로 변환
+    def get_wordidx_sequence(self, keywords):
+        if self.word_index is None:
+            return []
+        w2i = []
+        for word in keywords:
+            try:
+                w2i.append(self.word_index[word])
+            except KeyError:
+                # 해당 단어가 사전에 없는 경우 OOV 처리
+                w2i.append(self.word_index['OOV'])
+        return w2i
